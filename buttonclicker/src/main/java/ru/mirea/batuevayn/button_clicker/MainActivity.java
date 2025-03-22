638c0e4021f8a7673422f487a70b8a8ae7733688
Julia <batuewa.iulia@gmail.com>
2025-03-22 07:34:01 +0300

    first commit

---
+++ buttonclicker/src/main/java/ru/mirea/batuevayn/button_clicker/MainActivity.java
@@ -0,0 +1,50 @@
+package ru.mirea.batuevayn.button_clicker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvOut;
+    private Button btnWhoAmI;
+    private CheckBox checkBoxIsItMe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        tvOut = findViewById(R.id.tvOut);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        checkBoxIsItMe = findViewById(R.id.checkBox);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvOut.setText("Мой номер по списку № 3");
+                checkBoxIsItMe.setChecked(true);
+            }
+        };
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+    }
+
+    public void btnItIsNotMe(View view) {
+        Toast.makeText(this, "Это не я сделала", Toast.LENGTH_SHORT).show();
+        tvOut.setText("Hello World!");
+        checkBoxIsItMe.setChecked(false);
+    }
+}
